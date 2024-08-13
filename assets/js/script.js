0b634b71243137db3b6b7f593619a7d8ed4bf271
Miroslav Kopecký <53955446+MiroKopecky@users.noreply.github.com>
2024-08-14 00:26:22 +0200

    dev start

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+VANTA.BIRDS({
+    el: "#birds",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0xffffff,
+    backgroundAlpha: 0,
+    color1: 0x436a2b,
+    color2: 0x9f7f53,
+    colorMode: "lerp",
+    birdSize: 1.00,
+    wingSpan: 18.00,
+    speedLimit: 3.00,
+    separation: 80.00,
+    alignment: 55.00,
+    cohesion: 35.00,
+    quantity: 3.00
+})
+
+
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+	scrollHorizontally: true
+});
