24093bb783a8756ceb9ff277f945e867143fd3dd
Miroslav Kopecký <53955446+MiroKopecky@users.noreply.github.com>
2024-11-20 22:48:03 +0100

    mobile menu change

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SISKA</title>
+    <title>palosiska</title>
 
     <meta name="description" content="Content goes here...">
     <meta name="keywords" content="HTML, CSS, JavaScript">
